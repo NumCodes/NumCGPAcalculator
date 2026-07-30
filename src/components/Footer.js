19d9df89f2d169a8c9593562e50efae385ba2b7c
Nweze Ugochukwu Matthew <166723978+NumCodes@users.noreply.github.com>
2026-07-30 12:27:02 +0100

    Initial commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,15 +11,15 @@ function Icon({ icon, url }) {
 function Footer() {
     return (
         <footer className='text-center bg-gray-900 w-full py-3 text-white '>
-            Built by @Josh596
+            Built by @Num Codes
             <div className='flex justify-center gap-3 mt-2'>
-                <Icon icon={<BsTwitter size={18} />} url={'https://twitter.com/olujosh596'} />
-                <Icon icon={<BsGithub size={18} />} url={'https://github.com/Josh596'} />
-                <Icon icon={<BsLinkedin size={18} />} url={'https://www.linkedin.com/in/joshua-olukotun-a371761b2/'} />
+                <Icon icon={<BsTwitter size={18} />} url={'https://x.com/CodesNum80638'} />
+                <Icon icon={<BsGithub size={18} />} url={'https://github.com/numcodes'} />
+                <Icon icon={<BsLinkedin size={18} />} url={'https://www.linkedin.com/in/ugochukwu-nweze-08812a2b8/'} />
 
             </div>
             <div>
-                Source code for this project is available <a className='underline' target='_blank' rel="noreferrer" href='https://github.com/Josh596/cgpa-calculator'>here</a>.
+                Source code for this project is available <a className='underline' target='_blank' rel="noreferrer" href='https://github.com/NumCodes/NumCGPAcalculator'>here</a>.
             </div>
         </footer>
     )
